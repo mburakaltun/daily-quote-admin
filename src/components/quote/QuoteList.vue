2323e11f6f5c93596637d0cfcff5ef92c1931522
mburakaltun <mburakaltun@gmail.com>
2025-02-14 20:22:28 +0300

    deployment devs

--- src/components/quote/QuoteList.vue
+++ src/components/quote/QuoteList.vue
@@ -4,13 +4,15 @@
     <table class="w-full border-collapse">
       <thead>
       <tr class="text-black border-b-2 border-black">
-        <th class="p-2 font-bold text-app-text" style="width: 70%;">Quote</th>
+        <th class="p-2 font-bold text-app-text" style="width: 10%;">ID</th>
+        <th class="p-2 font-bold text-app-text" style="width: 60%;">Quote</th>
         <th class="p-2 font-bold text-app-text" style="width: 15%;">Author</th>
         <th class="p-2 font-bold text-app-text" style="width: 15%;">Actions</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="quote in quotes" :key="quote.id" class="transition-all border-b-2 border-black">
+        <td class="p-2 text-app-text text-center">{{ quote.id }}</td>
         <td class="p-2 text-app-text text-center">{{ quote.content }}</td>
         <td class="p-2 text-app-text text-center">{{ quote.author }}</td>
         <td class="p-2 text-app-text text-center">
