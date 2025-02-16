c658a3907fecd7f23eb410900cbcf7629f298efe
mburakaltun <mburakaltun@gmail.com>
2025-02-16 17:15:20 +0300

    refactoring devs

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/',
+  base: '/daily-quote-admin/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
